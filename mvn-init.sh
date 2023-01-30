687a67236e7df98640ef8c4561c074ff936edcff
AlainBeckerCPE <alain.becker@cpe.fr>
2023-01-30 00:07:05 +0000

    basic mvn init script

---
+++ mvn-init.sh
@@ -0,0 +1,8 @@
+# @see https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
+mvn archetype:generate \
+  -DarchetypeArtifactId=maven-archetype-quickstart \
+  -DarchetypeVersion=1.4 \
+  -DinteractiveMode=false \
+  -DgroupId=fr.cpe.cpe2223 \
+  -DartifactId=ics3filrouge \
+  -Dpackage=fr.cpe.cpe2223.ics3.filrouge
