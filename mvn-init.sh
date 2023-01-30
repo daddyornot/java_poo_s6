3a14b1526c0ef2d8e2e12540ec79908d289ba229
AlainBeckerCPE <alain.becker@cpe.fr>
2023-01-30 00:28:54 +0000

    create project skeleton in project root directory

--- mvn-init.sh
+++ mvn-init.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+CPE_ARTEFACT_ID=ics2filrouge2
+CPE_PACKAGE=fr.cpe.$CPE_ARTEFACT_ID
+
 # @see https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
 mvn archetype:generate \
   -DarchetypeArtifactId=maven-archetype-quickstart \
   -DarchetypeVersion=1.4 \
   -DinteractiveMode=false \
   -DgroupId=fr.cpe.cpe2223 \
-  -DartifactId=ics3filrouge \
-  -Dpackage=fr.cpe.cpe2223.ics3.filrouge
+  -DartifactId=$CPE_ARTEFACT_ID \
+  -Dpackage=$CPE_PACKAGE
+# @see https://stackoverflow.com/questions/24399613/using-maven-archetype-generate-in-the-same-directory
+echo "Moving artefact files to current directory"
+mv ./$CPE_ARTEFACT_ID/* .
+echo "Deleting $CPE_ARTEFACT_ID sub-directory"
+rmdir ./$CPE_ARTEFACT_ID
+echo "Done"
