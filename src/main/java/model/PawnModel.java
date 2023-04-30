3cfb625a0dd1f44170f132edbde01b490ab48895
AlainBeckerCPE <alain.becker@cpe.fr>
2023-04-30 04:41:12 +0200

    factor common behaviors of queens and pawns into a new abstract class

--- src/main/java/model/PawnModel.java
+++ src/main/java/model/PawnModel.java
@@ -1,58 +1,17 @@
 package model;
 
 
-import java.util.LinkedList;
-import java.util.List;
-
 import nutsAndBolts.PieceSquareColor;
 
-public class PawnModel implements PieceModel{
-
-	private Coord coord;
-	private PieceSquareColor pieceColor;
+public class PawnModel extends AbstractPieceModel {
 	
 	private int direction;
 
 	public PawnModel(Coord coord, PieceSquareColor pieceColor) {
-		super();
+		super(coord, pieceColor);
 
-		this.coord = coord;
-		this.pieceColor = pieceColor;
 		this.direction = PieceSquareColor.BLACK.equals(this.getPieceColor()) ? -1 : 1;
 
-	}
-
-	@Override
-	public char getColonne() {
-		return coord.getColonne();
-	}
-
-	@Override
-	public int getLigne() {
-		return coord.getLigne();
-	}
-
-	@Override
-	public boolean hasThisCoord(Coord coord) {
-		return this.coord.equals(coord);
-	}
-
-	@Override
-	public PieceSquareColor getPieceColor() {
-		return pieceColor;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return " [" + pieceColor.toString().charAt(0) + coord + "]";
-	}
-
-	@Override
-	public void move(Coord coord) {
-		this.coord = coord; 
 	}
 
 	@Override
@@ -81,31 +40,6 @@ public class PawnModel implements PieceModel{
 		}
 
 		return ret;
-	}
-
-	@Override
-	public List<Coord> getCoordsOnItinerary(Coord targetCoord) {
-
-		List<Coord> coordsOnItinery = new LinkedList<Coord>(); 
-
-		int initCol = this.getColonne();
-		int initLig = this.getLigne();
-		int colDistance = targetCoord.getColonne() - this.getColonne();
-		int ligDistance = targetCoord.getLigne() - this.getLigne();
-		int deltaLig = (int) Math.signum(ligDistance);
-		int deltaCol = (int) Math.signum(colDistance);
-
-		// Vérif déplacement en diagonale
-		if (Math.abs(colDistance) == Math.abs(ligDistance)){
-
-			// recherche coordonnées des cases traversées
-			for (int i = 1; i < Math.abs(colDistance); i++) {
-				Coord coord = new Coord((char) (initCol + i*deltaCol), initLig + i*deltaLig);
-				coordsOnItinery.add(coord);
-			}
-		}
-
-		return coordsOnItinery;
 	}
 
 	
--- src/main/java/model/QueenModel.java
+++ src/main/java/model/QueenModel.java
@@ -1,60 +1,18 @@
 package model;
 
 
-import java.util.LinkedList;
-import java.util.List;
-
 import nutsAndBolts.PieceSquareColor;
 /**
  * @author francoiseperrin
  *
  *le mode de déplacement et de prise de la reine est différent de celui du pion
  */
-public class QueenModel implements PieceModel {
-
-	private Coord coord;
-	private PieceSquareColor pieceColor;
+public class QueenModel extends AbstractPieceModel {
 
 	public QueenModel(Coord coord, PieceSquareColor pieceColor) {
-		super();
-
-		this.coord = coord;
-		this.pieceColor = pieceColor;
+		super(coord, pieceColor);
 	}
 
-	@Override
-	public char getColonne() {
-		return coord.getColonne();
-	}
-	
-	@Override
-	public int getLigne() {
-		return coord.getLigne();
-	}
-	
-	@Override
-	public boolean hasThisCoord(Coord coord) {
-		return this.coord.equals(coord);
-	}
-
-	@Override
-	public PieceSquareColor getPieceColor() {
-		return pieceColor;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return " [" + pieceColor.toString().charAt(0) + coord + "]";
-	}
-
-	@Override
-	public void move(Coord coord) {
-		this.coord = coord; 
-	}
-	
 	@Override
 	public boolean isMoveOk(Coord targetCoord, boolean isPieceToCapture) {
 		boolean ret = false;
@@ -68,31 +26,6 @@ public class QueenModel implements PieceModel {
 		}
 		
 		return ret;
-	}
-
-	@Override
-	public List<Coord> getCoordsOnItinerary(Coord targetCoord) {
-
-		List<Coord> coordsOnItinery = new LinkedList<Coord>(); 
-		
-		int initCol = this.getColonne();
-		int initLig = this.getLigne();
-		int colDistance = targetCoord.getColonne() - this.getColonne();
-		int ligDistance = targetCoord.getLigne() - this.getLigne();
-		int deltaLig = (int) Math.signum(ligDistance);
-		int deltaCol = (int) Math.signum(colDistance);
-
-		// Vérif déplacement en diagonale
-		if (Math.abs(colDistance) == Math.abs(ligDistance)){
-
-			// recherche coordonnées des cases traversées
-			for (int i = 1; i < Math.abs(colDistance); i++) {
-				Coord coord = new Coord((char) (initCol + i*deltaCol), initLig + i*deltaLig);
-				coordsOnItinery.add(coord);
-			}
-		}
-		
-		return coordsOnItinery;
 	}
 
 
